49526122df1ed9faaa919519cf327610676e8551
Neelothpala22 <neelojayalath@gmail.com>
2025-03-29 22:57:27 -0700

    Update

--- backend/src/config/database.config.js
+++ backend/src/config/database.config.js
@@ -11,9 +11,9 @@ set('strictQuery', true);
 
 export const dbconnect = async () => {
     try {
-        connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
+        await connect(process.env.MONGO_URI||'mongodb+srv://dbuser:mern@mernapp.tiima.mongodb.net/MernDB', {
+            // useNewUrlParser: true,
+            // useUnifiedTopology: true,
         });
         await seedUsers();
         await seedFoods();
